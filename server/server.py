671922b516547b90b9f625f20a01b25194f5c695
Alexander Bobkov <alexander.e.bobkov@gmail.com>
2015-11-17 01:46:23 +0300

    Scene clips testing

--- server/server.py
+++ server/server.py
@@ -1,7 +1,9 @@
 import os
 import os.path
 import psycopg2
+import subprocess
 from osgeo import gdal
+from osgeo import osr
 from zipfile import ZipFile
 from flask import Flask, request, redirect, url_for
 from werkzeug import secure_filename
@@ -93,10 +95,45 @@ def sceneclip(sceneid, minband, maxband):
     if not extract_bands(sceneid, minband, maxband):
         return 'NO FILE'
 
-    output = 'SUCCESS\n'
+    extractfolder = SCENES_EXTRACT_FOLDER + "/" + sceneid
+
+    output = 'SUCCESS_CLIP\n'
+
+    filename = sceneid[:23] + "B{0:0>3}_L1T.TIF".format(minband)
+
+    dataset = gdal.Open(extractfolder + "/" + filename, gdal.GA_ReadOnly)
+    if dataset is None:
+        return 'FAILED OPEN FILE'
+
+    projection = dataset.GetProjectionRef()
+    if projection is None:
+        return 'PROJECTION IS NULL'
+
+    srsOut = osr.SpatialReference()
+    if srsOut.ImportFromWkt(projection) != gdal.CE_None:
+        return 'FAILED IMPORT SRS'
+
+    srsIn = osr.SpatialReference()
+    srsIn.ImportFromEPSG(4326)
+
+    left = -94.857154026968
+    right = -94.806204078584
+    up = 31.588149083258
+    down = 31.519174227538
+
+    ct = osr.CoordinateTransformation(srsIn, srsOut)
+    (xLeft, yUp, h) = ct.TransformPoint(left, up)
+    (xRight, yDown, h) = ct.TransformPoint(right, down)
+
+    #print("XL = %d YU = %d XR = %d YD = %d" % (xLeft, yUp, xRight, yDown))
+
     for i in range(minband, maxband + 1):
-        filename = sceneid[:23] + "B{0:0>3}_L1T.TIF".format(i)
-        output += 'http://virtualglobe.ru/geoportal/hyperion/scenes/' + sceneid + '/' + filename + '\n'
+        inFilename = extractfolder + "/" + sceneid[:23] + "B{0:0>3}_L1T.TIF".format(i)
+        outFilename = extractfolder + "/" + sceneid[:23] + "B{0:0>3}_L1T_out.TIF".format(i)
+
+        subprocess.call(["gdal_translate", "-projwin", '{0}'.format(xLeft), '{0}'.format(yUp), '{0}'.format(xRight), '{0}'.format(yDown), inFilename, outFilename])
+
+        #output += 'http://virtualglobe.ru/geoportal/hyperion/scenes/' + sceneid + '/' + filename + '\n'
 
     return output
 
