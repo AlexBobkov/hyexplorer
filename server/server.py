d32c1a63633cb15310e6be80eb64c13a5fc85686
Alexander Bobkov <alexander.e.bobkov@gmail.com>
2015-12-05 17:37:02 +0300

    Updated server

--- server/server.py
+++ server/server.py
@@ -21,6 +21,7 @@ app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 1 * 1024 * 1024 #1mb
 
+
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
@@ -35,23 +36,25 @@ def overview(sceneid):
     app.logger.info('Overview %s %s', sceneid, request.method)
 
     if request.method == 'POST':
-        for name, file in request.files.items():
-            app.logger.info('File %s ', file.filename)
+	app.logger.info('POST')
+    
+	file = request.files['file']
+        app.logger.info('File %s ', file.filename)
 
-            if allowed_file(file.filename):
-                filename = secure_filename(file.filename)
-                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
 
-                #conn = psycopg2.connect("host=localhost dbname=GeoPortal user=user password=user")
-                conn = psycopg2.connect("host=178.62.140.44 dbname=GeoPortal user=portal password=PortalPass")
+            #conn = psycopg2.connect("host=localhost dbname=GeoPortal user=user password=user")
+            conn = psycopg2.connect("host=178.62.140.44 dbname=GeoPortal user=portal password=PortalPass")
 
-                cur = conn.cursor()
-                cur.execute("update scenes set hasoverview=TRUE, overviewname=%s where sceneid=%s;", (filename, sceneid))
-                conn.commit()
-                cur.close()
-                conn.close()
+            cur = conn.cursor()
+            cur.execute("update scenes set hasoverview=TRUE, overviewname=%s where sceneid=%s;", (filename, sceneid))
+            conn.commit()
+            cur.close()
+            conn.close()
 
-                app.logger.info('Scene is ready %s', sceneid)
+            app.logger.info('Scene is ready %s', sceneid)
 
         return 'Success POST'
     else:
@@ -188,7 +191,7 @@ def sceneclip(sceneid, minband, maxband):
     return output
 
 if __name__ == '__main__':
-    #app.debug = True
+    app.debug = True
 
     if not app.debug:
         import logging
