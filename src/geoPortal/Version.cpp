0e01dfe20c48d8b9bce5b4046f991ce801f7b35c
Alexander Bobkov <alexander.e.bobkov@gmail.com>
2016-08-23 00:49:15 +0300

    Updated version and removed unused boost libs

--- src/geoPortal/Version.cpp
+++ src/geoPortal/Version.cpp
@@ -30,5 +30,5 @@
 
 unsigned int portal::Version::majorVersion = 0;
 unsigned int portal::Version::minorVersion = 3;
-unsigned int portal::Version::patchVersion = 0;
-const char* portal::Version::str = "0.3.0";
+unsigned int portal::Version::patchVersion = 1;
+const char* portal::Version::str = "0.3.1";
